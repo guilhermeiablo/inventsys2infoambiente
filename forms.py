f1e4c464ef9b739c6b18246f460695ace4e472f0
Guilherme Iablonovski <guilhermeiablo@gmail.com>
2020-09-02 16:07:00 -0300

    flask

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, Selec
 from wtforms.validators import DataRequired, Length
 
 class LoginFormInventsys(FlaskForm):
-	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+	username = StringField('Usuário', validators=[DataRequired(), Length(min=2, max=20)])
 	password = PasswordField('Senha', validators=[DataRequired()])
 	remember = BooleanField('Salvar meus dados')
 	submit= SubmitField('Enviar') 
